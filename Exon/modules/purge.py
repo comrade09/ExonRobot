bd2662e8d9850e3428ba0aee67f6ccdd8ab396e2
comrade09 <106185020+comrade09@users.noreply.github.com>
2022-12-25 16:56:19 +0530

    Update purge.py

--- Exon/modules/purge.py
+++ Exon/modules/purge.py
@@ -1,18 +1,14 @@
 """
 MIT License
-
 Copyright (c) 2022 Aʙɪsʜɴᴏɪ
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -38,29 +34,25 @@ async def purge_messages(event):
     if event.from_id is None:
         return
 
-    if (
-        not await user_is_admin(
-            user_id=event.sender_id,
-            message=event,
-        )
-        and event.from_id not in DEV_USERS
-    ):
-        await event.reply("Only Admins are allowed to use this command")
+    if not await user_is_admin(
+        user_id=event.sender_id,
+        message=event,
+    ) and event.from_id not in [1087968824]:
+        await event.reply("ᴏɴʟʏ ᴀᴅᴍɪɴs ᴀʀᴇ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to purge the message")
+        await event.reply("ᴄᴀɴ'ᴛ sᴇᴇᴍ ᴛᴏ ᴘᴜʀɢᴇ ᴛʜᴇ ᴍᴇssᴀɢᴇ")
         return
 
     reply_msg = await event.get_reply_message()
     if not reply_msg:
-        await event.reply("Purged succesfully.")
+        await event.reply("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴇʟᴇᴄᴛ ᴡʜᴇʀᴇ ᴛᴏ sᴛᴀʀᴛ ᴘᴜʀɢɪɴɢ ғʀᴏᴍ.")
         return
-    messages = []
     message_id = reply_msg.id
     delete_to = event.message.id
 
-    messages.append(event.reply_to_msg_id)
+    messages = [event.reply_to_msg_id]
     for msg_id in range(message_id, delete_to + 1):
         messages.append(msg_id)
         if len(messages) == 100:
@@ -71,37 +63,35 @@ async def purge_messages(event):
         await event.client.delete_messages(event.chat_id, messages)
     except:
         pass
-    time_ = time.perf_counter() - start
-    text = f"Purged Successfully in {time_:0.2f} Second(s)"
+    time.perf_counter() - start
+    text = "f"shhh !! Purged Successfully in {time_:0.2f} Second(s)"
     await event.respond(text, parse_mode="markdown")
+
 
 async def delete_messages(event):
     if event.from_id is None:
         return
 
-    if (
-        not await user_is_admin(
-            user_id=event.sender_id,
-            message=event,
-        )
-        and event.from_id not in DEV_USERS
-    ):
-        await event.reply("Only Admins are allowed to use this command")
+    if not await user_is_admin(
+        user_id=event.sender_id,
+        message=event,
+    ) and event.from_id not in [1087968824]:
+        await event.reply("ᴏɴʟʏ ᴀᴅᴍɪɴs ᴀʀᴇ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("Can't seem to delete this?")
+        await event.reply("ᴄᴀɴ'ᴛ sᴇᴇᴍ ᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛʜɪs?")
         return
 
     message = await event.get_reply_message()
     if not message:
-        await event.reply("Whadya want to delete?")
+        await event.reply("ᴡʜᴀᴛ, ᴡᴀɴᴛ ᴛᴏ ᴅᴇʟᴇᴛᴇ?")
         return
     chat = await event.get_input_chat()
     del_message = [message, event.message]
     await event.client.delete_messages(chat, del_message)
 
-  
+
 PURGE_HANDLER = purge_messages, events.NewMessage(pattern="^[!/]spurge$")
 DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
 
