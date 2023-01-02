543bde9433544ef4fdb5fbfaa5c2234f0905ba4e
comrade09 <106185020+comrade09@users.noreply.github.com>
2023-01-03 01:12:05 +0530

    Update helper.py

--- ABG/helper.py
+++ ABG/helper.py
@@ -1,6 +1,6 @@
-HELP_IMG = ["https://telegra.ph/file/38a4ca19d4e1dfd1d8e7e.jpg"]
+HELP_IMG = ["https://telegra.ph/file/286230dd39b81f4aa84c7.jpg"]
 
 START_IMG = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
 
 
-PHOTO = ["https://telegra.ph/file/195be370e7c1d20ff4188.jpg"]
+PHOTO = ["https://telegra.ph/file/286230dd39b81f4aa84c7.jpg"]
